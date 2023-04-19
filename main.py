1cf8145eba5b23c931513d1c8605e9f3bd46ea56
Vera <verataranova29@gmail.com>
2023-04-19 03:41:52 +0300

    add sort function call in main file and edited sort function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from define_if_prime import is_prime
+from sort_sequence import sort
 
 if __name__ == '__main__':
     run = True
     while run:
-        print("1) Define is number prime or not\n0) Exit")
+        print("1) Define is number prime or not\n2) Sort sequence\n0) Exit")
         choice = input(">> ")
 
         if choice == "1":
@@ -16,5 +17,9 @@ if __name__ == '__main__':
                     print ("Number is not prime")
             else:
                 print("Your number is out of range")
+        elif choice == "2":
+            seq = [20, 7, 14, 9, 0, -4, 15, 81, 6, -17]
+            print("Unsorted sequence: ", seq)
+            print ("Sorted sequence: ", sort(seq))
         elif choice == "0":
             run = False
--- sort_sequence.py
+++ sort_sequence.py
@@ -2,12 +2,9 @@ def sort(sequence):
     length = len(sequence)
     if length == 0:
         return []
-    tmp = sequence[0]
     for i in range(0, length):
         for j in range(i + 1, length):
             if sequence[j] < sequence[i]:
-                tmp = sequence[j]
-                sequence[j] = sequence[i]
-                sequence[i] = tmp
+                sequence[j], sequence[i] = sequence[i], sequence[j]
     return sequence
 
